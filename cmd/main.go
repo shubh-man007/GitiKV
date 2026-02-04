81f2869634be56fb8966afca6691bb52d6262371
shubh-man007 <randombro2077@gmail.com>
2026-02-04 20:24:01 +0530

    Added proper type format

--- cmd/main.go
+++ cmd/main.go
@@ -1,40 +1,54 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"sync"
 )
 
 type GitiKV struct {
 	mu   sync.Mutex
-	Giti map[string]any
+	Giti map[string][]byte
 }
 
 func NewGitiKV() *GitiKV {
 	return &GitiKV{
-		Giti: map[string]any{},
+		Giti: map[string][]byte{},
 	}
 }
 
-func (gkv *GitiKV) Read(key string) any {
+func (gkv *GitiKV) Read(key string) ([]byte, bool) {
 	gkv.mu.Lock()
-	val := gkv.Giti[key]
-	gkv.mu.Unlock()
+	defer gkv.mu.Unlock()
+	val, ok := gkv.Giti[key]
 
-	return val
+	return val, ok
 }
 
-func (gkv *GitiKV) Write(key string, val any) {
+func (gkv *GitiKV) Write(key string, val []byte) {
 	gkv.mu.Lock()
 	gkv.Giti[key] = val
 	gkv.mu.Unlock()
 }
 
+func encodeInt32(i int32) []byte {
+	b := make([]byte, 4)
+	binary.BigEndian.PutUint32(b, uint32(i))
+	return b
+}
+
+func decodeInt32(b []byte) int32 {
+	return int32(binary.BigEndian.Uint32(b))
+}
+
 func main() {
 	gkv := NewGitiKV()
-	gkv.Write("workerID", 1)
-	gkv.Write("Price", 1)
 
-	val := gkv.Read("Price")
-	fmt.Printf("Price: %v\n", val)
+	gkv.Write("userID", encodeInt32(2))
+	val, ok := gkv.Read("userID")
+	if !ok {
+		panic("key not found")
+	}
+
+	fmt.Printf("userID: %d\n", decodeInt32(val))
 }
